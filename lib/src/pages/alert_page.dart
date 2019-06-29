17eed8e57f9ae99c10738d7ac012e9a18017eede
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-06-29 17:18:53 -0500

    Adding a custom AlertDialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,6 +8,45 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Alert Page"),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Show Alert"),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _showAlert(context),
+        ),
+      ),
+    );
+  }
+
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Title here"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text("This is the content"),
+              FlutterLogo(size: 100.0)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancel"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text("OK"),
+              onPressed: (){},
+            )
+          ],
+        );
+      }
     );
   }
 }
