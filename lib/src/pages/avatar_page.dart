9d21918d32b32d9d932ac689af7a71deb6ba4799
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-06-29 17:34:11 -0500

    Working on AvatarPage. adding two mini circles to the navbar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(3.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage("https://avatars0.githubusercontent.com/u/39273205?s=400&u=feaabf0bd850b7d59547b9a9dd7ff59ff21a2d31&v=4"),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 16),
+            child: CircleAvatar(
+              child: Text("DR"),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Stan_Lee_by_Gage_Skidmore_3.jpg/220px-Stan_Lee_by_Gage_Skidmore_3.jpg"),
+          placeholder: AssetImage("assets/jar-loading.gif"),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
