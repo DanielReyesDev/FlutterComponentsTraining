0828a8e874b3863c8eb4d7cff639b322d1b621e5
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-07-01 08:57:00 -0500

    Basic example finished

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -7,7 +7,20 @@ class ListPage extends StatefulWidget {
 
 class _ListPageState extends State<ListPage> {
 
-  List<int> _numbersList = [1,2,3,4,5];
+  List<int> _numbersList = new List();
+  int _lastItem = 0;
+  ScrollController _scrollController = new ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _addImages();
+    _scrollController.addListener((){
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        _addImages();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +34,7 @@ class _ListPageState extends State<ListPage> {
 
   Widget _buildList(){
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _numbersList.length,
       itemBuilder: (BuildContext context, int index) {
         final int image = _numbersList[index];
@@ -30,6 +44,18 @@ class _ListPageState extends State<ListPage> {
         );
       },
     );
+  }
+
+
+  void _addImages() {
+    for (var i = 0; i < 10; i++) {
+      _lastItem++;
+      _numbersList.add(_lastItem);
+    }
+
+    setState(() {
+      
+    });
   }
 
 
